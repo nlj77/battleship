b271973513590d30f347f68686b4fb97f36c3843
Nick Jones <nickjones9605@gmail.com>
2022-05-17 11:28:16 -0600

    Add cell class

---
+++ lib/cell.rb
@@ -0,0 +1,28 @@
+class Cell
+
+  #Create an attribute reader for coordinate, ship, and fired upon.
+
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = initialize
+    @fired_upon = false
+  end
+
+  def empty?
+    @ship.nil?
+  end
+#Creates and returns a ship
+  def place_ship(ship)
+    @ship = Ship
+  end
+
+  #Creates a fire upon method that also establishes fired_upon. If there is a S
+  #ship in the cell, then call hit method from ship.
+
+  def fire_upon
+    return "This cell has already been fired upon" if fired_upon?
+    @fired_upon = true
+    if @ship != nil
+      @ship.hit
+    end
+  end
