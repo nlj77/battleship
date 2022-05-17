95548bdeaba526aa32e3e046616b6daeb5633b70
Ben Silverstein <benjsilverstein@comcast.net>
2022-05-17 15:41:45 -0600

    Feat - Cell: Add attr_reader, redefine empty?, fix fire_upon

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,6 +1,7 @@
 class Cell
 
   #Create an attribute reader for coordinate, ship, and fired upon.
+  attr_reader :coordinate, :ship, :fired_upon
 
   def initialize(coordinate)
     @coordinate = coordinate
@@ -10,17 +11,23 @@ class Cell
 
   def empty?
     @ship.nil?
+
+    # if @ship == nil
+    #   true
+    # else false
+    # end
   end
 #Creates and returns a ship
   def place_ship(ship)
-    @ship = Ship
+    @ship = ship
+
   end
 
   #Creates a fire upon method that also establishes fired_upon. If there is a S
   #ship in the cell, then call hit method from ship.
 
   def fire_upon
-    return "This cell has already been fired upon" if fired_upon?
+    return "This cell has already been fired upon" if fired_upon
     @fired_upon = true
     if @ship != nil
       @ship.hit
