3a2cd9edc29d404d00a7b0a56f470d01757a914b
Ben Silverstein <benjsilverstein@comcast.net>
2022-05-17 11:58:44 -0600

    Test - Cell: ship, empty?, coordinate, place_ship

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -14,3 +14,31 @@ RSpec.describe Cell do
 
     expect(cell.coordinate).to eq("B4")
   end
+
+  it 'checks if ship is in cell' do
+    cell = Cell.new("B4")
+
+    expect(cell.ship).to eq(nil)
+  end
+
+  it 'checks if cell is empty' do
+    cell = Cell.new("B4")
+
+    expect(cell.empty?).to eq(true)
+  end
+
+  it 'has an assigned coordinate' do
+    cell = Cell.new("B4")
+
+    expect(cell.coordinate).to eq("B4")
+  end
+
+  it 'can place_ship' do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+
+    expect(cell.ship).to be_instance_of(Ship)
+  end
+  
+end
