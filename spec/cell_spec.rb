f354c7fe81d4b1b5b24e4cf82da253336d5ccf08
Ben Silverstein <benjsilverstein@comcast.net>
2022-05-17 12:31:55 -0600

    Test - Cell: fire_upon interaction with fired_upon? and ship health

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -48,6 +48,54 @@ RSpec.describe Cell do
 
     expect(cell.empty?).to eq(true)
   end
+#Second part of cell interaction pattern is bellow
 
+  it 'can place_ship' do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+
+    expect(cell.fired_upon).to eq(false)
+  end
+
+  it 'checks if ship was fired_upon' do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+
+    expect(cell.fired_upon).to eq(false)
+  end
+
+  # Fire upon should substract 1 health
+  it 'fire_upon ship' do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    cell.fire_upon
+
+    expect(cell.ship.health).to eq(2)
+  end
+
+  it 'fire_upon modifies fired_upon?' do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    cell.fire_upon
+    cell.ship.health
+
+    expect(cell.fired_upon).to eq(true)
+  end
+
+  it 'fire_upon modifies fired_upon?' do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    cell.fire_upon
+    cell.ship.health
+
+    expect(cell.fired_upon).to eq(true)
+  end
+
+#Third part of cell interaction pattern is bellow
 
 end
